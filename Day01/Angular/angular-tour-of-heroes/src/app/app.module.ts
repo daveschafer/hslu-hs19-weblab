24013e11ba25ab6218372a16a04faf8aba7a51bd
David Schafer <lemeper@gmail.com>
2020-02-04 09:18:08 +0100

    HeroService implemented

--- Day01/Angular/angular-tour-of-heroes/src/app/app.module.ts
+++ Day01/Angular/angular-tour-of-heroes/src/app/app.module.ts
@@ -3,16 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeroesComponent } from './heroes/heroes.component';
+import { HeroesComponent } from './heroes/heroes.component'; //wurde beim generating der component automatisch  deklariert
 //brauchen wir für 2way binding
-import { FormsModule } from '@angular/forms';
-import { HeroDetailComponent } from './hero-detail/hero-detail.component'; // 
+import { FormsModule } from '@angular/forms'; // 
+
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
